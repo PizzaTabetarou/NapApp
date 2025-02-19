a9569a0c22422f9423c516a0ae1e206e6fefb4fc
PizzaTabetarou <pot.gms@gmail.com>
2025-02-19 22:51:51 +0900

    コメント追加

--- TimerUIManager.cs
+++ TimerUIManager.cs
@@ -4,43 +4,50 @@ using TMPro;
 
 public class TimerUIManager : MonoBehaviour
 {
-    public TMP_InputField hoursInput;
-    public TMP_InputField minutesInput;
-    public TMP_Text countdownText;
-    public Button setButton;
-    public Button startButton;
-    public Button stopButton;
-    public GameObject rawImage;
-    public TimerCTRL timerCTRL;
-    public MusicPlayer musicPlayer;
-    public GameObject inputGroup;
-    public GameObject countdownGroup;
+    public TMP_InputField hoursInput;   //時間入力フィールド
+    public TMP_InputField minutesInput; //分入力フィールド
+    public TMP_Text countdownText;      //カウントダウン用テキスト
+    public Button setButton;            //タイマーセット用ボタン
+    public Button startButton;          //タイマー開始ボタン
+    public Button stopButton;           //タイマー停止ボタン
+    public GameObject rawImage;         //タイマー開始後暗くするよう
+    public TimerCTRL timerCTRL;         //TimerCTRLスクリプト
+    public MusicPlayer musicPlayer;     //MusicPlayerスクリプト
+    public GameObject inputGroup;       //タイマー設定用のパネル
+    public GameObject countdownGroup;   //カウントダウン用のパネル
     
-    private int totalMinutes = 0;
-    private bool isRunning = false;
+    private int totalMinutes = 0;       //時間(分に直す)
+    private bool isRunning = false;     //カウントしているかどうかの真偽値
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;  //android端末が、スリープモードに入らないようにするためのものらしい
         countdownGroup.SetActive(false);
         rawImage.SetActive(false);
 
+        //インプットフィールドに初期値『0』を入れておく
         hoursInput.text = "0";
         minutesInput.text = "0";
 
+        //AddListenerはButtonコンポーネントのメソッド
+        //引数として、クリックされたときに実行したいメソッドを指定する
         setButton.onClick.AddListener(SetTimer);
         startButton.onClick.AddListener(OnStartButtonPressed);
         stopButton.onClick.AddListener(OnStopButtonPressed);
 
+        //PlayAlarmをイベントハンドラーとして登録
         timerCTRL.OnTimerFinished += PlayAlarm;
     }
 
     void Update()
     {
+        //残り時間を分と秒で表示
         float time = timerCTRL.countdownTime;
         int minutes = Mathf.FloorToInt(time / 60);
         int seconds = Mathf.FloorToInt(time % 60);
+
+        //文字列補間により、可読性アップ
         countdownText.text = $"{minutes:D2}分{seconds:D2}秒";
         
         if(time is 0)
@@ -51,10 +58,13 @@ public class TimerUIManager : MonoBehaviour
 
     void SetTimer()
     {
+        //入力された文字列を整数に変換し、int ○○に代入
         if(int.TryParse(hoursInput.text, out int hours) && int.TryParse(minutesInput.text, out int minutes))
         {
+            //入力された時間を全て分に変換する
             totalMinutes = hours * 60 + minutes;
 
+            //timerCTRL.ResetTimerに変換された時間を渡す
             if(totalMinutes > 0)
             {
                 timerCTRL.ResetTimer(totalMinutes * 60);
